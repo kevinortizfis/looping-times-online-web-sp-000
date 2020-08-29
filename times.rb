d2c4af0afcb89adef8aee68677efddc177f1760f
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-08-29 21:34:28 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,10 +1,5 @@
-times = 1
-def using_times(times)
-until times == 7
-  times+= 1
+def using_times
+	7.times do
+    puts  "Wingardium Leviosa" 
+  end
 end
-puts times
-puts  "Wingardium Leviosa"
-end
-
-using_times(times)
